8b6a4d725eb7b59bd4d7b376b9a11946c0fe2aea
laesteticautomotiva-cmyk <la.esteticautomotiva@gmail.com>
2026-04-21 11:34:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-const express = require("express");
-const axios = require("axios");
-
-const app = express();
-app.use(express.json());
-
-app.get("/test", (req, res) => {
-  res.json({ ok: true });
-});
-
 app.get("/webhook", (req, res) => {
-  const verify_token = process.env.VERIFY_TOKEN;
-
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === verify_token) {
+  const VERIFY_TOKEN = "laestetica123";
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
     return res.status(200).send(challenge);
   }
 
   return res.sendStatus(403);
 });
-
-app.post("/webhook", (req, res) => {
-  console.log("WEBHOOK:", req.body);
-  res.sendStatus(200);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("rodando na porta", PORT));
